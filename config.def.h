fac0b2b492a573aa424e559bec3e01e1b7d1fbcd
inugami-dev64 <jxwest@protonmail.com>
2021-04-12 21:06:13 +0300

    Change webbrowser bindings

--- config.def.h
+++ config.def.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
@@ -82,9 +82,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,          zoom,               {0} },
 	{ MODKEY,                       XK_Tab,             view,               {0} },
 	{ MODKEY|ShiftMask,             XK_c,               killclient,         {0} },
-	{ MODKEY,                       XK_t,               setlayout,          {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,               setlayout,          {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,               setlayout,          {.v = &layouts[2]} },
+	{ MODKEY,                       XK_a,               setlayout,          {.v = &layouts[0]} },
+	{ MODKEY,                       XK_s,               setlayout,          {.v = &layouts[1]} },
+	{ MODKEY,                       XK_d,               setlayout,          {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,           setlayout,          {0} },
 	{ MODKEY|ShiftMask,             XK_space,           togglefloating,     {0} },
 	{ MODKEY,                       XK_0,               view,               {.ui = ~0 } },
